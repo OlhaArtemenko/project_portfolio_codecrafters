e91c668ba2d59426e37b05b50c0f33bd7bfaf8ed
NataliiaBybyk <natashakorobochka@gmail.com>
2024-04-12 12:09:43 +0200

    add about-me css and js

---
+++ src/js/about-me.js
@@ -0,0 +1,47 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+
+function itemsAccordion() {
+  const buttonAccordion = document.querySelector('.accordion-button-up-down');
+
+  new Accordion('.accordion-info-list', {
+    duration: 700,
+    showMultiple: true,
+    openOnInit: [0],
+    onOpen: currentElement => {
+      currentElement.querySelector('.ac-trigger').classList.add('button-roll');
+    },
+    onClose: currentElement => {
+      currentElement
+        .querySelector('.ac-trigger')
+        .classList.remove('button-roll');
+    },
+  });
+}
+
+function addAboutMeSwiper() {
+  const swiper = new Swiper('.skills-container', {
+    modules: [Navigation, Keyboard, Mousewheel],
+
+    navigation: {
+      nextEl: '.skills-button-next',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    breakpoints: {
+      375: { slidesPerView: 2 },
+      768: { slidesPerView: 3 },
+      1440: {
+        slidesPerView: 6,
+      },
+    },
+    mousewheel: {
+      invert: true,
+    },
+    loop: true,
+  });
+}
