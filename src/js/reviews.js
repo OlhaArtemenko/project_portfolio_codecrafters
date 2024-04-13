28db02f5ef8bf3779e38a7b66b73210eb2afd091
alexandrataranets <142220751+alexandrataranets@users.noreply.github.com>
2024-04-13 11:57:29 +0200

    update reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,3 +1,31 @@
+import iziToast from 'izitoast';
 import Swiper from 'swiper';
 import 'swiper/css';
 import { getReviews } from './api.js';
+
+
+function renderReviews(reviews, reviewList, ok) {
+  if (ok) {
+    const reviewHTML = reviews
+      .map(
+        ({ author, avatar_url, review }) =>
+          `<li class="reviews-list-item swiper-slide">
+            <img 
+              class="reviews-person-img"
+              src="${avatar_url}"
+              alt="${author} photo"
+              width="48"
+              height="48"
+              loading="lazy">
+            <div class="reviews-text-content">
+              <h3 class="reviews-person-name">${author}</h3>
+              <p class="reviews-text-p">${review}</p>
+            </div>
+          </li>`
+      )
+      .join('');
+    reviewList.innerHTML = reviewHTML;
+  } else {
+    reviewList.innerHTML = `<h3>Not found :(</h3>`;
+  }
+}
