16f8d5f523d56f2ad6571f3a5fa4b360a5597abd
Diana Kharchenko <vitadiana2@gmail.com>
2024-04-13 15:34:19 +0300

    правки от ментора 400

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,104 +1,40 @@
-// const form = document.querySelector('.form');
-// form.addEventListener('submit', handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-
-//   const formData = new FormData(document.getElementById('form'));
-
-//   fetch('https://portfolio-js.b.goit.study/api/requests', {
-//     method: 'POST',
-//     body: formData,
-//   })
-//     .then(response => {
-//       if (response.ok) {
-//         document.getElementById('modalW').classList.remove('visually-hidden');
-//         document.addEventListener('keydown', function(event) {
-//           if (event.key === 'Escape') {
-//             document.getElementById('modalW').classList.add('visually-hidden');
-//           }
-//         });
-//         const modalBtn = document.querySelector('.modal-button');
-//         modalBtn.addEventListener('click', function() {
-//           document.getElementById('modalW').classList.add('visually-hidden');
-//         });
-//         form.reset();
-//       } else {
-//         alert('An error occurred when sending it. Please try again.');
-//       }
-//     })
-//     .catch(error => {
-//       alert('An error occurred when sending it. Please check your internet connection.');
-//     });
-// }
-
-
-// import axios from 'axios';
-// import { getRequest } from './api.js';
-
-// const form = document.querySelector('.form');
-// form.addEventListener('submit', handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-
-//   const formData = new FormData(document.getElementById('form'));
-
-//   axios.post('https://portfolio-js.b.goit.study/api/requests', formData)
-//     .then(response => {
-//       if (response.status === 200) {
-//         document.getElementById('modalW').classList.remove('visually-hidden');
-//         document.addEventListener('keydown', function(event) {
-//           if (event.key === 'Escape') {
-//             document.getElementById('modalW').classList.add('visually-hidden');
-//           }
-//         });
-//         const modalBtn = document.querySelector('.modal-button');
-//         modalBtn.addEventListener('click', function() {
-//           document.getElementById('modalW').classList.add('visually-hidden');
-//         });
-//         form.reset();
-//       } else {
-//         alert('An error occurred when sending it. Please try again.');
-//       }
-//     })
-//     .catch(error => {
-//       alert('An error occurred when sending it. Please check your internet connection.');
-//     });
-// }
-
-
-
 import axios from 'axios';
-
 const form = document.querySelector('.form');
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  const formData = new FormData(document.getElementById('form'));
+  const email = document.querySelector('.input-footer[type="email"]').value;
+  const message = document.getElementById('message').value;
 
-  axios.post('https://portfolio-js.b.goit.study/api/requests', formData)
+  const formData = {
+    email: email,
+    message: message,
+  };
+
+  axios
+    .post('https://portfolio-js.b.goit.study/api/requests', formData)
     .then(response => {
       if (response.status === 200) {
         document.getElementById('modalW').classList.remove('visually-hidden');
-        document.addEventListener('keydown', function(event) {
+        document.addEventListener('keydown', function (event) {
           if (event.key === 'Escape') {
             document.getElementById('modalW').classList.add('visually-hidden');
           }
         });
         const modalBtn = document.querySelector('.modal-button');
-        modalBtn.addEventListener('click', function() {
+        modalBtn.addEventListener('click', function () {
           document.getElementById('modalW').classList.add('visually-hidden');
         });
         form.reset();
       } else {
-
         throw new Error('An error occurred when sending it. Please try again.');
       }
     })
     .catch(error => {
-      alert('An error occurred when sending it. Please check your internet connection.');
+      alert(
+        'An error occurred when sending it. Please check your internet connection.'
+      );
     });
 }
