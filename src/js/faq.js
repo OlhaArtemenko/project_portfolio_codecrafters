fc8f9c516ac6201df04cb04cad914436592a7eb2
Olha Art <global14072012@gmail.com>
2024-04-14 14:03:50 +0300

    faq acor

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,40 +1,46 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
+const btnAcc = document.querySelectorAll('.ac-trigger');
 
-
-const faqList = document.querySelector('.faq-list');
-const faqItems = document.querySelectorAll('.faq-li');
-const faqButtons = document.querySelectorAll('.fqa-btn-open');
-
-
-function toggleAccordion(event) {
-    
-    const faqItem = event.currentTarget.closest('.faq-li');
-    
-    const panel = faqItem.querySelector('.panel');
-
-    
-    faqItem.classList.toggle('active');
-
-
-    if (panel.style.maxHeight === '0px' || panel.style.maxHeight === '') {
-        panel.style.maxHeight = panel.scrollHeight + 'px';
-    } else {
-        panel.style.maxHeight = '0';
-    }
-}
-
-
-faqButtons.forEach(button => {
-    button.addEventListener('click', toggleAccordion);
+new Accordion('.faq-list', {
+  duration: 700,
+  showMultiple: false,
+  onOpen: currentElement => {
+    currentElement.querySelector('.ac-trigger').classList.add('button-roll');
+  },
+  onClose: currentElement => {
+    currentElement.querySelector('.ac-trigger').classList.remove('button-roll');
+  },
 });
 
+// const faqList = document.querySelector('.faq-list');
+// const faqItems = document.querySelectorAll('.faq-li');
+// const faqButtons = document.querySelectorAll('.fqa-btn-open');
 
-faqItems.forEach(faqItem => {
-    const panel = faqItem.querySelector('.panel');
-   
-    panel.style.maxHeight = '0';
-    panel.style.overflow = 'hidden';
-    panel.style.transition = 'max-height 0.7s ease'; 
-});
+// function toggleAccordion(event) {
+
+//     const faqItem = event.currentTarget.closest('.faq-li');
+
+//     const panel = faqItem.querySelector('.panel');
+
+//     faqItem.classList.toggle('active');
+
+//     if (panel.style.maxHeight === '0px' || panel.style.maxHeight === '') {
+//         panel.style.maxHeight = panel.scrollHeight + 'px';
+//     } else {
+//         panel.style.maxHeight = '0';
+//     }
+// }
+
+// faqButtons.forEach(button => {
+//     button.addEventListener('click', toggleAccordion);
+// });
+
+// faqItems.forEach(faqItem => {
+//     const panel = faqItem.querySelector('.panel');
+
+//     panel.style.maxHeight = '0';
+//     panel.style.overflow = 'hidden';
+//     panel.style.transition = 'max-height 0.7s ease';
+// });
