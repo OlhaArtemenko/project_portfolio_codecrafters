3340197f4e13fe54c11dba457e5ab7a8bbedee1a
NataliiaBybyk <natashakorobochka@gmail.com>
2024-04-14 16:57:50 +0200

    add about me

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -21,7 +21,7 @@ new Accordion('.accordion-info-list', {
 const swiper = new Swiper('.skills-container', {
   modules: [Navigation, Keyboard, Mousewheel],
   loop: true,
-  speed: 700,
+  speed: 600,
   grabCursor: true,
   allowTouchMove: true,
   navigation: {
@@ -41,8 +41,25 @@ const swiper = new Swiper('.skills-container', {
     invert: false,
   },
   on: {
-    reachEnd: function () {
-      this.slideNext();
+    init: function () {
+      const activeIndex = this.activeIndex;
+      this.slides.forEach((slide, index) => {
+        if (index === activeIndex) {
+          slide.classList.add('active-slide');
+        } else {
+          slide.classList.remove('active-slide');
+        }
+      });
+    },
+    slideChange: function () {
+      const activeIndex = this.activeIndex;
+      this.slides.forEach((slide, index) => {
+        if (index === activeIndex) {
+          slide.classList.add('active-slide');
+        } else {
+          slide.classList.remove('active-slide');
+        }
+      });
     },
   },
 });
