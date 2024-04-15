eacef95f4e058e82d8472e2dd3b5f3565102089b
Olha Art <global14072012@gmail.com>
2024-04-15 04:00:15 +0300

    sw

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,36 +3,34 @@ import { Navigation, Pagination } from 'swiper/modules';
 
 import 'swiper/css';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const WorkSwiper = new Swiper('.projects .swiper', {
-    loop: false,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-      disabledClass: 'disabled',
-    },
-    modules: [Navigation, Pagination],
-  });
+const WorkSwiper = new Swiper('.project-swiper-div', {
+  loop: false,
+  speed: 800,
 
-  // Функція для встановлення стилів кнопок перемикання
-  function setButtonStyles() {
-    const prevButton = document.querySelector('.swiper-button-prev');
-    const nextButton = document.querySelector('.swiper-button-next');
-
-    const isPrevDisabled = prevButton.classList.contains('disabled');
-    const isNextDisabled = nextButton.classList.contains('disabled');
-
-    prevButton.style.color = isPrevDisabled ? '#fafafa33' : '#fafafa';
-    prevButton.style.borderColor = isPrevDisabled ? '#fafafa33' : '#fafafa';
-
-    nextButton.style.color = isNextDisabled ? '#fafafa33' : '#fafafa';
-    nextButton.style.borderColor = isNextDisabled ? '#fafafa33' : '#fafafa';
-  }
-
-  setButtonStyles();
-
-  // Додаємо обробник події для встановлення стилів при зміні слайда
-  WorkSwiper.on('slideChange', function () {
-    setButtonStyles();
-  });
+  navigation: {
+    nextEl: '.project-swiper-button-next',
+    prevEl: '.project-swiper-button-prev',
+    disabledClass: 'disabled',
+  },
+  modules: [Navigation, Pagination],
 });
+
+// Функція для встановлення стилів кнопок перемикання
+function setButtonStyles() {
+  const prevButton = document.querySelector('.project-swiper-button-prev');
+  const nextButton = document.querySelector('.project-swiper-button-next');
+
+  const isPrevDisabled = prevButton.classList.contains('disabled');
+  const isNextDisabled = nextButton.classList.contains('disabled');
+
+  prevButton.style.color = isPrevDisabled ? '#fafafa33' : '#fafafa';
+  prevButton.style.borderColor = isPrevDisabled ? '#fafafa33' : '#fafafa';
+
+  nextButton.style.color = isNextDisabled ? '#fafafa33' : '#fafafa';
+  nextButton.style.borderColor = isNextDisabled ? '#fafafa33' : '#fafafa';
+}
+
+setButtonStyles();
+
+prevButton.addEventListener('click', setButtonStyles);
+nextButton.addEventListener('click', setButtonStyles);
