41b88c6133e25d730bcb13597b71d99c4fd80c88
Olha Art <global14072012@gmail.com>
2024-04-15 22:29:12 +0300

    styles

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -20,6 +20,7 @@ new Accordion('.accordion-info-list', {
 
 const swiper = new Swiper('.skills-container', {
   modules: [Navigation, Keyboard, Mousewheel],
+  slidesPerView: 'auto',
   loop: true,
   speed: 700,
   grabCursor: true,
@@ -45,9 +46,9 @@ const swiper = new Swiper('.skills-container', {
       const activeIndex = this.activeIndex;
       this.slides.forEach((slide, index) => {
         if (index === activeIndex) {
-          slide.classList.add('active-slide');
+          slide.classList.add('active');
         } else {
-          slide.classList.remove('active-slide');
+          slide.classList.remove('active');
         }
       });
     },
@@ -55,11 +56,23 @@ const swiper = new Swiper('.skills-container', {
       const activeIndex = this.activeIndex;
       this.slides.forEach((slide, index) => {
         if (index === activeIndex) {
-          slide.classList.add('active-slide');
+          slide.classList.add('active');
         } else {
-          slide.classList.remove('active-slide');
+          slide.classList.remove('active');
         }
       });
     },
   },
+});
+// Add event listener for 'next' button
+const nextButton = document.querySelector('.skills-button-next');
+nextButton.addEventListener('click', function () {
+  const visibleSlides = swiper.slides.filter(slide => slide.isVisible());
+  const isEnd = swiper.isEnd && visibleSlides.length === swiper.slides.length;
+
+  if (isEnd || visibleSlides.length === swiper.slides.lengthVisible) {
+    swiper.slideTo(0); // Перехід до першого елемента
+  } else {
+    swiper.slideNext(); // Виклик методу slideNext() для переміщення наступного слайда
+  }
 });
